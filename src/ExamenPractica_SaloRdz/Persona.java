09bb5798c0b831562421c0d8933576968fbeaa12
RodriguezSalos <rodriguez.salos@gmail.com>
2021-03-26 15:47:26 -0600

    Add Setter Getters

--- src/ExamenPractica_SaloRdz/Persona.java
+++ src/ExamenPractica_SaloRdz/Persona.java
@@ -1,4 +1,24 @@
 package ExamenPractica_SaloRdz;
 
 public class Persona {
+    private String nombre;
+    private String apellido;
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+
 }
