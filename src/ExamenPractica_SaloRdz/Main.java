bef9d08eb8ef1ed441b1de3517a09f06549256d9
RodriguezSalos <rodriguez.salos@gmail.com>
2021-03-26 22:18:49 -0600

    add lines

--- src/ExamenPractica_SaloRdz/Main.java
+++ src/ExamenPractica_SaloRdz/Main.java
@@ -3,7 +3,22 @@ package ExamenPractica_SaloRdz;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("hola mundo");
+
+        Persona obj1 = new Persona("Salo");
+
+        obj1.setApellido("Rodriguez");
+
+        obj1.imprimirNombre();
+
+        /*
+         * Crea una Entidad Empleado con lo siguiente: 2 atributos de tipo cadena,
+         * nombre y apellido con sus respectivos getters y setters,
+         * 
+         * un constructor que acepte el nombre de la persona como parámetro de entrada,
+         * 
+         * finalmente agrega un Comportamiento llamado imprimirNombre y que imprima el
+         * nombre y el apellido.
+         */
+
     }
 }
